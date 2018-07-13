a2a5fbb26c4ee2af0640d7edc259a5be2b7f4e40
Dude <craig.boman@gmail.com>
2018-07-13 10:48:15 -0400

    shelflist update

--- sql/create_shelflist.sql
+++ sql/create_shelflist.sql
@@ -1,7 +1,12 @@
-SELECT 
+﻿SELECT 
 -- i.inventory_gmt, 
-lower(p.barcode) as barcode, upper(p.call_number_norm || COALESCE(' ' || v.field_content, '') ) as call_number_norm, i.location_code, i.item_status_code,
-s.content AS inventory_note, to_date(c.due_gmt::text, 'YYYY-MM-DD HH:MI:SS') as due_gmt
+lower(p.barcode) as barcode, 
+upper(p.call_number_norm || COALESCE(' ' || v.field_content, '') ) as call_number_norm, 
+b.best_title,
+i.location_code, 
+i.item_status_code,
+s.content AS inventory_note, to_date(c.due_gmt::text, 
+'YYYY-MM-DD HH:MI:SS') as due_gmt
 
 FROM 
 sierra_view.item_record_property	AS p 
@@ -30,12 +35,17 @@ sierra_view.bib_record_item_record_link AS l
 ON
   l.item_record_id = i.id
 
+LEFT JOIN
+sierra_view.bib_record_property as b
+ON
+  b.bib_record_id = l.bib_record_id
+
 WHERE 
-i.location_code = 'rc4'
+i.location_code = 'imr'
 AND 
-p.call_number_norm >= lower('DA   86 R52') 
+p.call_number_norm >= lower('BL  312 R45 2010') 
 AND 
-p.call_number_norm <= lower('DA  506 A2 A4')
+p.call_number_norm <= lower('Z  1039 G55 I83 2016')
 
 -- since we have the situation where multiple bibs can share the same item record, we should remove duplicated items.
 -- we need to tweak this ... not sure grouping is the best way to do this.
